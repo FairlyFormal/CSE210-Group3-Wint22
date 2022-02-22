036c4509d7938e74cb7efcd75a2a5ca1a0cffa8d
Joe Hawkins <hawkins@byui.edu>
2022-02-22 13:11:58 -0700

    finished encapsulation project

--- jumper/Commander.py
+++ jumper/Commander.py
@@ -1,35 +1,90 @@
 import Words
-from Terminal_Butler import Terminal
+import Terminal_Butler
 import Stickman
 
 
-#RESPONSIBILITIES:
-# Starts the game, Ends the game, Communicated with Terminal to determine the end of the game.
-
-#Start game - Initialize the three classes (Word, stickman, and terminal).
-
-#Ends Game - Once a guess has been made the Terminal will check every turn to see if the guess affects lives or if the word has been guessed.
 
 
 class Commander():
 
 
     def __init__(self):
-        self.terminal = Terminal()
-        pass
+
+        self.terminal = Terminal_Butler.Terminal()
+        self.stickman = Stickman.Stickman()
+        self.words = Words.Words()
+        self.input = ''
+        self.inplay = True
+        self.lost = False
+
 
 
     
-    def start_game(self): #Initializes all the processes from the different classes.
+    def game_loop(self):
 
-        Terminal.print_hidden_word()
-        #Prints the hidden word in _____ underscores.
-        Stickman.draw_stickman() #Draws the stickman.
-        Terminal.user_guess #Prompts the user to input a letter.
+        while self.inplay:
+
+            self.terminal.print_guessed_word(self.words.guessed_word)
+            self.stickman.draw_stickman()
+            self.get_input()
+            self.replace_letters()
+            self.is_end_game()
+        
+
+        if self.lost:
+
+            self.terminal.print_lose_screen()
+        
+        else:
+
+            self.terminal.print_win_screen()
 
 
-    def end_game(self): #Checks with terminal to see if user has guessed the word or has lost all their lives.
+        
 
-        print('Thanks for playing.')
+    def get_input(self):
+
+        user_input = input('Enter a letter [a-z]: ')    
+        self.input = user_input
 
 
+    def replace_letters(self):
+
+        index = 0
+        found_letter = False
+
+        for letter in self.words.hidden_word:
+
+
+            if self.input == letter:
+
+                found_letter = True
+                self.words.guessed_word[index] = self.input
+
+            index += 1
+
+        if not found_letter:
+
+            self.stickman.lives -= 1
+            self.stickman.remove_section()
+
+
+
+
+
+
+
+    def is_end_game(self):
+
+        if self.stickman.lives == 0: 
+            
+            self.inplay = False
+            self.lost = True
+
+        
+        elif not "_" in self.words.guessed_word:
+            
+            self.inplay = False
+            self.lost = False
+
+        
--- jumper/Stickman.py
+++ jumper/Stickman.py
@@ -1,19 +1,10 @@
 class Stickman():
 
 
-    #There is 3 parts to this class:
-
-    #The first part contains a list with the stickman parts (What Hunter did yesterday in class).
-
-    #The second part is drawing the stickman, it will print (line by line) every character in the list. This draws the picture of the stickman.
-
-    #The third part is removing a section of the stickman, removing a life.
-
-
     def __init__(self):
         
-        self.stickman = [""] #Put the stickman parts within the list here. HUNTER KNOWS HOW TO DO THIS. I COULDN'T FIND THE PARTS.
-        self.lives = 0 #The amount of stickman parts is the same as the number of lives the stickman has. 0 is just a placeholder.
+        self.stickman = ['  ___  ',' /___\ ',' \   / ','  \ /  ','   0   ','  /|\  ','  / \   '] 
+        self.lives = 7
 
 
 
@@ -22,13 +13,11 @@ class Stickman():
         stickman = self.stickman
 
         for line in stickman:
-            
-            print(f"\n {stickman}")
+
+            print(line)
 
     
 
     def remove_section(self): #Removes a section and subtracts a life.
 
-        self.stickman.pop(0) #Removes the first element in the stickman list (This may be backwars, should probably check.)
-
-        return self.stickman() #Returns the updated Stickman.
+        self.stickman.pop(0)

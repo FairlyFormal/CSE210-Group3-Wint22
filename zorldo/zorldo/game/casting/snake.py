4c794970599618751d5529581981b42b6cb6c36a
IsaiMar <80867603+IsaiMar@users.noreply.github.com>
2022-03-29 19:58:58 -0600

    Update snake.py

--- zorldo/zorldo/game/casting/snake.py
+++ zorldo/zorldo/game/casting/snake.py
@@ -24,30 +24,19 @@ class Snake(Actor):
         # move all segments
         for segment in self._segments:
             segment.move_next()
-        # update velocities
-        for i in range(len(self._segments) - 1, 0, -1):
-            trailing = self._segments[i]
-            previous = self._segments[i - 1]
-            velocity = previous.get_velocity()
-            trailing.set_velocity(velocity)
 
     def get_head(self):
         return self._segments[0]
 
+    def move_zorldo(self):
+
+        x = (self._position.get_x() + self._velocity.get_x()) % 600
+        y = (self._position.get_y() + self._velocity.get_y()) % 900
+        self._segments = Point(x, y)
+        return self.position
+
     def grow_tail(self, number_of_segments):
         return
-        # for i in range(number_of_segments):
-        #     tail = self._segments[-1]
-        #     velocity = tail.get_velocity()
-        #     offset = velocity.reverse()
-        #     position = tail.get_position().add(offset)
-            
-        #     segment = Actor()
-        #     segment.set_position(position)
-        #     segment.set_velocity(velocity)
-        #     segment.set_text("#")
-        #     segment.set_color(constants.GREEN)
-        #     self._segments.append(segment)
 
     def turn_head(self, velocity):
         self._segments[0].set_velocity(velocity)
@@ -56,15 +45,14 @@ class Snake(Actor):
         x = int(constants.MAX_X / 2)
         y = int(constants.MAX_Y / 2)
 
-        for i in range(constants.SNAKE_LENGTH):
-            position = Point(x - i * constants.CELL_SIZE, y)
-            velocity = Point(1 * constants.CELL_SIZE, 0)
-            text = "('_')"
-            color = constants.YELLOW if i == 0 else constants.GREEN
+        position = Point(x - 1 * constants.CELL_SIZE, y)
+        velocity = Point(1, 0)
+        text = "('_')"
+        color = constants.YELLOW if 1 == 0 else constants.GREEN
             
-            segment = Actor()
-            segment.set_position(position)
-            segment.set_velocity(velocity)
-            segment.set_text(text)
-            segment.set_color(color)
-            self._segments.append(segment)
+        segment = Actor()
+        segment.set_position(position)
+        segment.set_velocity(velocity)
+        segment.set_text(text)
+        segment.set_color(color)
+        self._segments.append(segment)
